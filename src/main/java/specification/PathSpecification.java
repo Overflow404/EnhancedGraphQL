07e3bb731065881b8c91a7b9dd6c51d250537da0
ArmatoGa <gaspare.armato1@gmail.com>
2020-10-28 09:47:47 +0100

    Added list of attribute specification

--- src/main/java/specification/PathSpecification.java
+++ src/main/java/specification/PathSpecification.java
@@ -7,6 +7,7 @@ import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Path;
 import javax.persistence.criteria.Predicate;
+import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SetAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 
@@ -23,6 +24,10 @@ public interface PathSpecification<T> {
 
     default <S> Specification<S> atPath(final SingularAttribute<S, T> singularAttribute) {
         return (root, query, cb) -> toPredicate(root.join(singularAttribute), query, cb);
+    }
+
+    default <S> Specification<S> atPath(final ListAttribute<S, T> listAttribute) {
+        return (root, query, cb) -> toPredicate(root.join(listAttribute), query, cb);
     }
 
     @Nullable
